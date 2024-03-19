561a0e102e8060d9dcde32ad418e86fd8138c09e
Erick Ramirez <erickramirez.dnbk@gmail.com>
2024-03-19 15:21:54 -0300

    Añadir estilos y funcionalidad de arrastrar y soltar a las columnas y tareas

--- react-zustand-course/src/components/column.jsx
+++ react-zustand-course/src/components/column.jsx
@@ -1,17 +1,60 @@
+import { useState } from 'react'
 import { useStore } from '../store'
 import './column.css'
 import { Task } from './task'
+import classNames from 'classnames'
 
 export function Column({ state }) {
+  const [text, setText] = useState('')
+  const [open, setOpen] = useState(false)
+  const [drop, setDrop] = useState(false)
   const tasks = useStore((store) =>
     store.tasks.filter((task) => task.state === state)
   )
+  const addTask = useStore((store) => store.addTask)
+  const setDraggedTask = useStore((store) => store.setDraggedTask)
+  const draggedTask = useStore((store) => store.draggedTask)
+  const moveTask = useStore((store) => store.moveTask)
 
   return (
-    <div className='column'>
-      <p>{state}</p>
-      
-      <Task title={'Todo'} />
+    <div
+      className={classNames('column', { drop: drop })}
+      onDragOver={(e) => {
+        setDrop(true)
+        e.preventDefault()
+      }}
+      onDragLeave={(e) => {
+        setDrop(false)
+        e.preventDefault()
+      }}
+      onDrop={(e) => {
+        moveTask(draggedTask, state)
+        setDraggedTask(null)
+      }}
+    >
+      <div className='titleWrapper'>
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+      {tasks.map((task) => (
+        <Task key={task.title} title={task.title} />
+      ))}
+      {open && (
+        <div className='Modal'>
+          <div className='modelContent'>
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state)
+                setText('')
+                setOpen(false)
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- react-zustand-course/src/components/task.jsx
+++ react-zustand-course/src/components/task.jsx
@@ -1,15 +1,29 @@
 import classNames from 'classnames'
 import './task.css'
+import { useStore } from '../store'
 
-const STATUS = 'DONE'
+// const STATUS = 'DONE'
 
 export function Task({ title }) {
+  const task = useStore((store) =>
+    store.tasks.find((task) => task.title === title)
+  )
+  const setDraggedTask = useStore((store) => store.setDraggedTask)
+  const deleteTask = useStore((store) => store.deleteTask)
   return (
-    <div className='task'>
-      <div>{title}</div>
+    <div
+      className='task'
+      draggable
+      onDragStart={() => {
+        setDraggedTask(task.title)
+      }}
+    >
+      <div>{task.title}</div>
       <div className='bottomWrapper'>
-        <div></div>
-        <div className={classNames('status', STATUS)}>{STATUS}</div>
+        <div onClick={() => deleteTask(task.title)} className='delete'>
+          Delete
+        </div>
+        <div className={classNames('status', task.state)}>{task.state}</div>
       </div>
     </div>
   )
